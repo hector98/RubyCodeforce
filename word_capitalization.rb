9f4665f475864f9150d354b9110c598cd461efa4
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2023-06-23 21:45:24 -0600

    Resolver problema word_capitalization

---
+++ word_capitalization.rb
@@ -0,0 +1,11 @@
+# Codeforece
+# A. Word Capitalization
+
+# Leemos el texto de entrada y guradamos en la variable txt
+txt = gets.chomp
+
+# Ponemos la primera letra del texto en mayusculas
+txt[0] = txt[0].capitalize
+
+# mostramos el texto modificado
+puts txt
